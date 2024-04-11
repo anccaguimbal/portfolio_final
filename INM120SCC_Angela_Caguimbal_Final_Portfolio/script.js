3f8cf27c102b7a5746483751330be5a53f33dfed
anccaguimbal <160880585+anccaguimbal@users.noreply.github.com>
2024-04-11 09:47:38 -0400

    Add files via upload

---
+++ INM120SCC_Angela_Caguimbal_Final_Portfolio/script.js
@@ -0,0 +1,5 @@
+function personInfo (name, bio, occupation) {
+    console.log ('Hi! My name is Angela Caguimbal and I am an artist, pianist, and graphic designer. I work as a part-time associate at a retail company. Feel free to check my portfolio')
+}
+
+personInfo ('Angela Caguimbal', 'Artist', 'Pianist', 'Graphic Designer', 'Part-Time Associate')
